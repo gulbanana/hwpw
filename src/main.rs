bf34d0262183e46dbce1ba3f8b42dd19f6ca6eb5
Thomas Castiglione <castiglione@mac.com>
2024-06-30 22:48:50 +0800

    tricolour "pixel" controlled by multiple buttons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use embassy_futures::select::select;
 use embassy_rp::gpio::{Input, Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -8,20 +9,26 @@ use {defmt_rtt as _, panic_probe as _};
 async fn main(_spawner: embassy_executor::Spawner) {
     let io = embassy_rp::init(Default::default());    
     let mut btn_a = Input::new(io.PIN_12, embassy_rp::gpio::Pull::Up);
+    let mut btn_b = Input::new(io.PIN_13, embassy_rp::gpio::Pull::Up);
+    let mut btn_x = Input::new(io.PIN_15, embassy_rp::gpio::Pull::Up);
+    let mut btn_y = Input::new(io.PIN_16, embassy_rp::gpio::Pull::Up);
     let mut led_r = Output::new(io.PIN_6, Level::High);
     let mut led_g = Output::new(io.PIN_7, Level::High);
     let mut led_b = Output::new(io.PIN_8, Level::High);
 
     loop {
-        btn_a.wait_for_any_edge().await;
+        select(btn_a.wait_for_any_edge(), btn_b.wait_for_any_edge()).await;
+        
         if btn_a.is_high() {
-            led_r.set_high();
-            led_g.set_high();
             led_b.set_high();
         } else {
-            led_r.set_low();
-            led_g.set_low();
             led_b.set_low();
-        }        
+        }
+
+        if btn_b.is_high() {
+            led_r.set_high();
+        } else {
+            led_r.set_low();
+        }
     }
 }
